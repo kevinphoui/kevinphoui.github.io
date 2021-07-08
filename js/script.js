0f1dc3a5c0e61ede53cd39172b90be160dae439e
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2021-07-07 20:48:16 -0500

    CSS and Contact Update
    Added a map to Contact Me and adjusted website margin.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// JavaScript
+// JavaScript File
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -20,3 +20,19 @@ function showSlides(n) {
     
     slides[slideIndex-1].style.display = "block";
 }
+
+// Contact Me Map
+function initMap() {
+    // The location of East Bethel
+    const eastBethel = { lat: 45.3194, lng: -93.2024 };
+    // The map, centered at East Bethel
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 9,
+      center: eastBethel,
+    });
+    // The marker, positioned at East Bethel
+    const marker = new google.maps.Marker({
+      position: eastBethel,
+      map: map,
+    });
+  }
