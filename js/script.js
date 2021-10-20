83fe8492aed21d80bc8f7283b76efb319245bf69
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2021-10-20 14:04:59 -0500

    Added Static Images

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
 // JavaScript File
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex-1].style.display = "block";
-}
 
 // Contact Me Map
 function initMap() {
