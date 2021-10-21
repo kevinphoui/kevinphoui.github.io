5c0842a174f79a3f1b94182314b38d69b255bcfa
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2021-10-21 01:48:38 -0500

    Added a Top Button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // JavaScript File
 
+// Button to Top
+//Get the button
+var mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 // Contact Me Map
 function initMap() {
     // The location of East Bethel
@@ -15,3 +37,4 @@ function initMap() {
       map: map,
     });
   }
+
